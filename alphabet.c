f41ae0a22ae5ea910841ffdee6d985979ac9310e
PavithraChinna <47449773+PavithraChinna@users.noreply.github.com>
2019-02-08 18:43:33 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    
+    if(ch>='a'&& ch<='z' || ch>='A'&&ch<='Z')
+    {
+        printf("alphabet");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
