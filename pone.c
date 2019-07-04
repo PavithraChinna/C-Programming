77871ade58079f389d6e093d07567d6f99571eb2
PavithraChinna <47449773+PavithraChinna@users.noreply.github.com>
2019-07-04 19:02:19 +0530

    Update pone.c

--- pone.c
+++ pone.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-int main#include<stdio.h>
 int main()
 {
     int n;
     scanf("%d",&n);
-    if(n>0)
+    if(n==0)
+        printf("Zero");
+    else if(n>0)
     printf("Positive");
     else
     printf("Negative");
